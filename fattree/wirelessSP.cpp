09dc3770787ecf37a7f5cef35b67554d309182b4
guan <kevinx6000@gmail.com>
2015-07-23 12:37:44 +0800

    wired: no TCAM concern; wireless: pre-calculate with random BFS;

--- fattree/wirelessSP.cpp
+++ fattree/wirelessSP.cpp
@@ -5,15 +5,14 @@
 void Fattree::wirelessSP(void){
 
 	// IDs
-	int srcID, dstID, nowID;
-	double xx[2], yy[2];
+	int srcID, dstID, nowID, nxtID;
 
-	// A* BFS
+	// BFS randomly
 	int prev[pod*pod/2];
 	int sft = numberOfCore + numberOfAggregate;
-	priority_queue<myPair>pque;
-	myPair now, nxt;
-	memset(prev, 0, sizeof(prev));
+	int pickPort;
+	queue<int>BFS;
+	vector<int>ranPort;
 
 	// Paths
 	vector<int>rev;
@@ -30,38 +29,38 @@ void Fattree::wirelessSP(void){
 		// Self: empty path
 		this->wlPath[i].push_back(path);
 
-		// All pair destination
+		// Clear previous record
+		memset(prev, 0, sizeof(prev));
+
+		// BFS
 		srcID = sft + i;
+		BFS.push(srcID);
+		prev[srcID-sft] = srcID;
+		while(!BFS.empty()){
+			nowID = BFS.front();
+			BFS.pop();
+
+			// Generate port sequence
+			for(int j = 0; j < sw[nowID]->wlink.size(); j++)
+				ranPort.push_back(j);
+			for(int j = 0; j < sw[nowID]->wlink.size(); j++){
+
+				// Random pick one port
+				pickPort = rand()%((int)ranPort.size());
+				nxtID = sw[nowID]->wlink[ ranPort[pickPort] ].id;
+				if(!prev[nxtID-sft]){
+					prev[nxtID-sft] = nowID;
+					BFS.push(nxtID);
+				}
+
+				// Remove that port
+				ranPort.erase(ranPort.begin()+pickPort);
+			}
+		}
+
+		// All destination Path
 		for(int j = i+1; j < numberOfEdge; j++){
 			dstID = sft + j;
-			
-			// A* BFS
-			now.id = srcID;
-			now.hop = 0;
-			now.dis = 0.0;
-			prev[srcID-sft] = srcID;
-			pque.push(now);
-			while(!pque.empty()){
-				now = pque.top();
-				pque.pop();
-				for(int z = 0; z < sw[now.id]->wlink.size(); z++){
-					nxt.id = sw[now.id]->wlink[z].id;
-					if(!prev[nxt.id-sft]){
-						prev[nxt.id-sft] = now.id;
-						nxt.hop = now.hop+1;
-						xx[0] = sw[nxt.id]->posXY[0];
-						yy[0] = sw[nxt.id]->posXY[1];
-						xx[1] = sw[dstID]->posXY[0];
-						yy[1] = sw[dstID]->posXY[1];
-						nxt.dis = myDis(xx[0], yy[0], xx[1], yy[1]);
-						pque.push(nxt);
-					}
-					if(prev[dstID-sft]) break;
-				}
-				if(prev[dstID-sft]) break;
-			}
-
-			// Path
 			nowID = dstID;
 			while(nowID != srcID){
 				rev.push_back(nowID);
@@ -70,16 +69,14 @@ void Fattree::wirelessSP(void){
 			rev.push_back(srcID);
 			for(int z = rev.size()-1; z >= 0; z--)
 				path.push_back(rev[z]);
-			
+
 			// Row path
 			this->wlPath[i].push_back(path);
 			this->wlPath[j].push_back(rev);
-			
+
 			// Clear
 			path.clear();
 			rev.clear();
-			memset(prev, 0, sizeof(prev));
-			while(!pque.empty()) pque.pop();
 		}
 	}
 }
