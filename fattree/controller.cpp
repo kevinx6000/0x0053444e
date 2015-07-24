9b80fe57763f478ba0fbe7c854de67c388532625
guan <kevinx6000@gmail.com>
2015-07-24 16:20:31 +0800

    修改record flow policy; 移除controller sort event(hop數差異);

--- fattree/controller.cpp
+++ fattree/controller.cpp
@@ -28,6 +28,7 @@ void Fattree::controller(Event ctrEvt){
 	vector<Event>flowSetupEvent;
 	vector<Entry>vent;
 	bool hasHandle = false;
+	int k;
 
 	// Classify events
 	for(int i = 0; i < cumQue.size(); i++){
@@ -52,14 +53,72 @@ void Fattree::controller(Event ctrEvt){
 				nowFlowID = rcdFlowID[pkt];
 				vent.clear();
 
-				// Wireless policy or not at starting edge switch
-				if(allEntry[nowFlowID][0].isWireless() || nid != allEntry[nowFlowID][0].getSID() || !wireless(nid, pkt, vent, temp)){
+				// Extract original entry
+				if(rule(nid, allEntry[nowFlowID], ent)){
 
-					// Rule needed
-					if(rule(nid, allEntry[nowFlowID], ent)){
-						ent.setExpire(ctrEvt.getTimeStamp() + flowSetupDelay + ENTRY_EXPIRE_TIME);
+					// Update timestamp
+					ent.setExpire(ctrEvt.getTimeStamp() + flowSetupDelay + ENTRY_EXPIRE_TIME);
+					temp = ctrEvt.getTimeStamp() + flowSetupDelay + computePathDelay;
 
-						// Switch side install rule
+					// Starting edge switch, and has wireless or better wired path
+					if(nid == allEntry[nowFlowID][0].getSID() && (wireless(nid, pkt, vent, temp) || wired(nid, pkt, vent, temp))){
+
+						// Remove entries with the same switch ID (controller side)
+						for(int j = 0; j < vent.size(); j++){
+							temp = vent[j].getSID();
+							for(k = 0; k < allEntry[nowFlowID].size(); k++){
+								if(allEntry[nowFlowID][k].getSID() == temp){
+									allEntry[nowFlowID].erase(allEntry[nowFlowID].begin() + k);
+									k--;
+								}
+							}
+						}
+
+						// Add entries
+						for(int j = 0; j < vent.size(); j++)
+							allEntry[nowFlowID].insert(allEntry[nowFlowID].begin()+j, vent[j]);
+
+						// Remove entries with the same flow ID, if it has different output port (switch side)
+						for(int j = 0; j < vent.size(); j++){
+							temp = vent[j].getSID();
+							for(k = 0; k < sw[temp]->TCAM.size(); k++){
+								// Has rule for this flow
+								if(sw[temp]->TCAM[k].isMatch(pkt)){
+									if(sw[temp]->TCAM[k].getOutputPort() != vent[j].getOutputPort()){
+
+										/* No need to remove switch TCAM entry here, */
+										/* because install event will do this. */
+
+										// Install the new entry
+										ret.setEventType(EVENT_INSTALL);
+										ret.setTimeStamp(ctrEvt.getTimeStamp() + flowSetupDelay);
+										ret.setID(vent[j].getSID());
+										ret.setPacket(pkt);
+										ret.setEntry(vent[j]);
+										eventQueue.push(ret);
+									}
+									break;
+								}
+							}
+							// No rule for this flow
+							if(k == sw[temp]->TCAM.size()){
+								// Install the new entry
+								ret.setEventType(EVENT_INSTALL);
+								ret.setTimeStamp(ctrEvt.getTimeStamp() + flowSetupDelay);
+								ret.setID(vent[j].getSID());
+								ret.setPacket(pkt);
+								ret.setEntry(vent[j]);
+								eventQueue.push(ret);
+							}
+						}
+					}
+
+					/* Possible situations: */
+					/* 1: Any switch along the path */
+					/* 2: Starting edge swithc with no capacity available */
+					else{
+
+						// Install the original entry back to switch
 						ret.setEventType(EVENT_INSTALL);
 						ret.setTimeStamp(ctrEvt.getTimeStamp() + flowSetupDelay);
 						ret.setID(nid);
@@ -67,40 +126,13 @@ void Fattree::controller(Event ctrEvt){
 						ret.setEntry(ent);
 						eventQueue.push(ret);
 					}
-					// Rule not found
-					else{
-						/* This may cause by rule deletion of starting edge policy.*/
-						fprintf(stderr, "Rule extract failed!!!\n");
-					}
 				}
 
-				// Wired policy @ starting edge switch, and has wireless solution
+				// No entry for recorded flow???
 				else{
-
-					/* Remove entries along the path for this flowID */
-					for(int j = 0; j < allEntry[nowFlowID].size(); j++){
-						temp = allEntry[nowFlowID][j].getSID();
-						for(int k = 0; k < sw[temp]->TCAM.size(); k++)
-							if(sw[temp]->TCAM[k].isMatch(pkt)){
-								sw[temp]->TCAM.erase(sw[temp]->TCAM.begin()+k);
-								k--;
-							}
-					}
-
-					// Install rule
-					for(int j = 0; j < vent.size(); j++){
-
-						// Switch side event
-						ret.setEventType(EVENT_INSTALL);
-						ret.setTimeStamp(ctrEvt.getTimeStamp() + flowSetupDelay + computePathDelay);
-						ret.setID(vent[j].getSID());
-						ret.setPacket(pkt);
-						ret.setEntry(vent[j]);
-						eventQueue.push(ret);
-					}
-
-					/* Update allEntry */
-					allEntry[nowFlowID] = vent;
+					/* I don't know how this can happen... */
+					/* Maybe it's caused by rule deletion of starting edge policy? */
+					fprintf(stderr, "Error: extract original flow entry failed.\n");
 				}
 			}
 
@@ -118,10 +150,6 @@ void Fattree::controller(Event ctrEvt){
 	}
 	if(((int)cumQue.size()) > 0) hasHandle = true;
 	cumQue.clear();
-
-	// Sort with the largest gap between wired & wireless
-	mySort msrt(this);
-	sort(flowSetupEvent.begin(), flowSetupEvent.end(), msrt);
 
 	// Currently, all flow setup apply wired policy
 	for(int j = 0; j < flowSetupEvent.size(); j++){
