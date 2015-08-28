0f827dc8342717b1b9027c9ee7bf23027244f888
guan <kevinx6000@gmail.com>
2015-08-28 16:43:42 +0800

    加入active/inactive(並更新timeStamp)

--- fattree/updateTCAM.cpp
+++ fattree/updateTCAM.cpp
@@ -7,12 +7,22 @@ void Fattree::updateTCAM(int nid, int timeStamp){
 
 	Packet tmpPkt;
 
-	// The most front entries
-	while(!sw[nid]->TCAM.empty()){
-		if(sw[nid]->TCAM.front().isExpired(timeStamp)){
-			tmpPkt = sw[nid]->TCAM.front().getSample();
-			sw[nid]->TCAMmap.erase(tmpPkt);
-			sw[nid]->TCAM.pop_front();
+	// The most front entries (Active)
+	while(!sw[nid]->TCAMactive.empty()){
+		if(sw[nid]->TCAMactive.front().isExpired(timeStamp)){
+			tmpPkt = sw[nid]->TCAMactive.front().getSample();
+			sw[nid]->TCAMmapA.erase(tmpPkt);
+			sw[nid]->TCAMactive.pop_front();
+		}
+		else break;
+	}
+
+	// The most front entries (Inactive)
+	while(!sw[nid]->TCAMinactive.empty()){
+		if(sw[nid]->TCAMinactive.front().isExpired(timeStamp)){
+			tmpPkt = sw[nid]->TCAMinactive.front().getSample();
+			sw[nid]->TCAMmapI.erase(tmpPkt);
+			sw[nid]->TCAMinactive.pop_front();
 		}
 		else break;
 	}
